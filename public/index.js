ec95934fae43226bc81fd277e4f11bd5232c8602
Shadowrunner11 <pieroramirez810@gmail.com>
2024-05-16 22:55:10 -0500

    deleted node modules

--- public/index.js
+++ public/index.js
@@ -1,1 +1,39 @@
-document.addEventListener('securitypolicyviolation', console.log)
+
+const request = new Request('/reports', {
+  method: 'POST',
+  headers: new Headers({
+    'Content-Type': 'application/json'
+  })
+});
+
+const cspViolations = [];
+
+const debounce = (callback, wait) => {
+  let timeoutId = null;
+  return (...args) => {
+    window.clearTimeout(timeoutId);
+    timeoutId = window.setTimeout(() => {
+      callback.apply(null, args);
+    }, wait);
+  };
+}
+
+
+const debouncedFetch = debounce((data) => {
+  request.body = data;
+  fetch(request)
+}, 1500);
+
+document.addEventListener('securitypolicyviolation', (event)=>{
+  const data = {
+    violatedDirective:event.violatedDirective,
+    blockedURI: event.blockedURI,
+    effectiveDirective: event.effectiveDirective,
+    constructor: event.target.constructor?.name,
+    elementName: event.target.nodeName,
+  }
+
+  cspViolations.push(data);
+
+  debouncedFetch({cspViolations})
+})
--- server/src/index.mjs
+++ server/src/index.mjs
@@ -1,32 +1,45 @@
 import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
+import { writeFile } from 'fs/promises'
 import express, { json } from 'express';
+import papaparse from 'papaparse';
     
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
-app.use(json());
+app
+  .get('/reports', (req, res) => {
+    res.sendStatus(200);
+  });
 
-app.post('/reports', (req, res) => {
-  console.log('asdasdasd')
-  console.log(req.body);
-  res.sendStatus(200);
-});
-
-app.get('/reports', (req, res) => {
-  console.log('asdasdasd')
-  console.log(req.body);
-  res.sendStatus(200);
-});
 
 app
-  .use((_, res, next)=>{
-    res.set('Content-Security-Policy', ["default-src 'self'"])
+  .use(json())
+  .post('/reports', (req, res) => {
+    res.sendStatus(200);
+    const { cspViolations } = req.body ?? {};
+    
+  
+    const spanishData = cspViolations.map(report =>({
+      'Directiva violada': report.violatedDirective,
+      'URI bloqueada': report.blockedURI,
+      'Directiva efectiva': report.effectiveDirective,
+      'Constructor': report.constructor,
+      'Nombre del elemento': report.elementName,
+    }))
+  
+    writeFile(papaparse.unparse(spanishData));
+  });
 
-    next()
-  }, express.static(join(__dirname, '../../public')))
+
+app
+  .use(express.static(join(__dirname, '../../public'), {
+    setHeaders:()=>({
+        'Content-Security-Policy-Report-Only': 'default-src https:;'
+      })
+  }))
 ;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
